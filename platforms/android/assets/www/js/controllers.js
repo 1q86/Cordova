9822b6d93008d4efb1cc97831a79a402103bbb0b
1q86 <roc.p.wang@gmail.com>
2016-06-23 13:47:28 +0800

    demo

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -1,14 +1,14 @@
 angular.module('app.controllers', [])
   
-.controller('page2Ctrl', function($scope) {
+.controller('categoryCtrl', function($scope) {
 
 })
    
-.controller('page3Ctrl', function($scope) {
+.controller('authorCtrl', function($scope) {
 
 })
    
-.controller('page4Ctrl', function($scope) {
+.controller('downloadCtrl', function($scope) {
 
 })
          
@@ -16,7 +16,7 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('signupCtrl', function($scope) {
+.controller('signUpCtrl', function($scope) {
 
 })
    
@@ -29,6 +29,110 @@ angular.module('app.controllers', [])
 })
    
 .controller('feedbackCtrl', function($scope) {
+
+})
+   
+.controller('authorResultCtrl', function($scope) {
+
+})
+   
+.controller('paperListCtrl', function($scope) {
+
+})
+   
+.controller('categoryResultCtrl', function($scope) {
+
+})
+   
+.controller('singleChoiceCtrl', function($scope) {
+
+})
+   
+.controller('systemSetCtrl', function($scope) {
+
+})
+   
+.controller('mineCtrl', function($scope) {
+
+})
+   
+.controller('favouriteCtrl', function($scope) {
+
+})
+   
+.controller('wrongCtrl', function($scope) {
+
+})
+   
+.controller('statisticCtrl', function($scope) {
+
+})
+   
+.controller('tagCtrl', function($scope) {
+
+})
+   
+.controller('queryResultCtrl', function($scope) {
+
+})
+   
+.controller('subCategoryCtrl', function($scope) {
+
+})
+   
+.controller('paperSetCtrl', function($scope) {
+
+})
+   
+.controller('titleListCtrl', function($scope) {
+
+})
+   
+.controller('answerCtrl', function($scope) {
+
+})
+   
+.controller('shareAppCtrl', function($scope) {
+
+})
+   
+.controller('passwordCtrl', function($scope) {
+
+})
+   
+.controller('paperShareCtrl', function($scope) {
+
+})
+   
+.controller('queryListCtrl', function($scope) {
+
+})
+   
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('publishListCtrl', function($scope) {
+
+})
+   
+.controller('rankListCtrl', function($scope) {
+
+})
+   
+.controller('myPublishCtrl', function($scope) {
+
+})
+   
+.controller('rankByTitleCtrl', function($scope) {
+
+})
+   
+.controller('rankByCategoryCtrl', function($scope) {
+
+})
+   
+.controller('rankByAuthorCtrl', function($scope) {
 
 })
  
